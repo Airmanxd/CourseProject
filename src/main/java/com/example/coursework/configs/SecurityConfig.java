d9c0db04af052052adc89d1992ea87370d9d608b
Alex <airman13k@gmail.com>
2021-05-24 15:42:20 +0300

    added navbar and logout

--- src/main/java/com/example/coursework/configs/SecurityConfig.java
+++ src/main/java/com/example/coursework/configs/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
@@ -29,7 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/index", "/registration", "/login", "/products").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/**").hasRole("USER")
-                .and().formLogin().loginPage("/login");
+                .and().formLogin().loginPage("/login")
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/index");
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/com/example/coursework/controllers/HomeController.java
+++ src/main/java/com/example/coursework/controllers/HomeController.java
@@ -3,11 +3,17 @@ package com.example.coursework.controllers;
 import com.example.coursework.models.User;
 import com.example.coursework.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/")
@@ -37,6 +43,14 @@ public class HomeController {
     public String reg(User user) {
         userService.register(user);
         return "redirect:/products";
+    }
+
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/index";
     }
 
     @GetMapping("error")
